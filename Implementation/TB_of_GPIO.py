435a17dcbab27ed667cad49015779a0179df547a
Patrick <alexander.ivasca@yahoo.com>
2026-04-24 09:41:29 +0200

    Changed default python interpreter to virtual env

--- Implementation/TB_of_GPIO.py
+++ Implementation/TB_of_GPIO.py
@@ -6,9 +6,25 @@ from Lib.GPIO import *
 sys = py4hw.HWSystem()
 
 
-#mem = MemoryInterface(sys,'port0',8,16)
+Interface = MemoryInterface(sys,'port0',8,16)
 
-#Com = GPIO(sys,'GPIO',mem)
+Module = GPIO(sys,'GPIO',Interface)
+#read
+#read_data
+#address
+#write
+#write_data
+#be
+#resp
+
+py4hw.Sequence(sys,'read',[0,0,0,0,0,0,0,0,17],Interface.read)
+py4hw.Sequence(sys,'read_data',[0,0,0,0,0,0,0,0,1],Interface.read_data)
+py4hw.Sequence(sys,'address',[0,0,0,0,0,0,0,0,17],Interface.address)
+py4hw.Sequence(sys,'write',[0,0,0,0,0,0,0,0,1],Interface.write)
+py4hw.Sequence(sys,'write_data',[0,0,0,0,0,0,0,0,17],Interface.write_data)
+py4hw.Sequence(sys,'be',[0,0,0,0,0,0,0,0,1],Interface.be)
+py4hw.Sequence(sys,'be',[0,0,0,0,0,0,0,0,1],Interface.resp)
+
 
 sch = py4hw.Schematic(sys)
 sch.draw()
